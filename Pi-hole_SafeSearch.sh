1c17b8ef110b2a72b90af21abb16224c310a7574
nascosto <2653283+nascosto@users.noreply.github.com>
2020-10-04 18:54:12 -0600

    Use restrictmoderate everywhere for YouTube

--- Pi-hole_SafeSearch.sh
+++ Pi-hole_SafeSearch.sh
@@ -25,11 +25,11 @@ hostRecords=(
     "host-record=safesearch.pixabay.com,176.9.158.70"
 )
 ytSS=(
-   "cname=www.youtube.com,restrict.youtube.com"
-   "cname=m.youtube.com,restrict.youtube.com"
-   "cname=youtubei.googleapis.com,restrict.youtube.com"
-   "cname=youtube.googleapis.com,restrict.youtube.com"
-   "cname=www.youtube-nocookie.com,restrict.youtube.com"
+   "cname=www.youtube.com,restrictmoderate.youtube.com"
+   "cname=m.youtube.com,restrictmoderate.youtube.com"
+   "cname=youtubei.googleapis.com,restrictmoderate.youtube.com"
+   "cname=youtube.googleapis.com,restrictmoderate.youtube.com"
+   "cname=www.youtube-nocookie.com,restrictmoderate.youtube.com"
 )
 bingSS=(
     "cname=bing.com,www.bing.com,strict.bing.com"
